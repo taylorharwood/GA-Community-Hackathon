0ac853a9b5a996120f2f820a44d1be30232d1011
Meghan Joseph <majoseph77@aol.com>
2015-07-24 14:28:22 -0700

    model and controller started

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'events/index'
+
+  get 'events/new'
+
+  get 'events/show'
+
+  get 'events/create'
+
   root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ db/migrate/20150724212516_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.string :location
+      t.string :event_type
+      t.string :event_date
+      t.string :event_time
+      t.text :event_uri
+
+      t.timestamps null: false
+    end
+  end
+end
